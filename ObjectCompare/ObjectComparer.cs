b71c20a3fe824fe550b420625a9d0e947ebff39f
mrobertson <mrobertson@SFDEV050>
2020-07-29 13:34:28 -0400

    Add project files.

---
+++ ObjectCompare/ObjectComparer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectCompare
+{
+    public class ObjectComparer<T1, T2>
+    {
+
+        private readonly T1 Object1;
+        private readonly T2 Object2;
+
+        public List<ValuePair> Pairs { get; set; } = new List<ValuePair>();
+
+
+        /// <summary>
+        /// Initializes an ObjectComparer that compares all the field values of the two supplied objects
+        /// </summary>
+        /// <param name="object1"></param>
+        /// <param name="object2"></param>
+        public ObjectComparer(T1 object1, T2 object2)
+        {
+            Object1 = object1;
+            Object2 = object2;
+            var obj1Fields = object1.GetType().GetProperties().ToList();
+            var obj2Fields = object2.GetType().GetProperties().ToList();
+
+            foreach (var obj1Prop in obj1Fields)
+            {
+                var obj2Prop = obj2Fields.Where(p => p.Name == obj1Prop.Name).FirstOrDefault();
+                if (obj1Prop != null && obj2Prop != null)
+                    Pairs.Add(
+                        ValuePair.Create(
+                                                Object1.GetType().GetProperty(obj1Prop.Name).GetValue(Object1),
+                                                Object2.GetType().GetProperty(obj2Prop.Name).GetValue(Object2)
+                    ));
+            }
+        }
+
+        /// <summary>
+        /// Initializes an ObjectComparer that compares the two objects and defined mapped values supplied in 
+        /// the configuration.
+        /// </summary>
+        public ObjectComparer(Action<ObjectComparerConfiguration> configuration)
+        {
+            var config = new ObjectComparerConfiguration
+            {
+                Object1 = (T1)(object)null,
+                Object2 = (T2)(object)null,
+                Mappings = new List<ValuePair>()
+            };
+
+            configuration.Invoke(config);
+
+            Object1 = config.Object1;
+            Object2 = config.Object2;
+            Pairs.AddRange(config.Mappings.Select(pair => ValuePair.Create(pair.Prop1, pair.Prop2)));
+        }
+
+        public bool Compare()
+        {
+            return Pairs.All(p => p.ValuesEqual());
+        }
+
+
+        public class ObjectComparerConfiguration
+        {
+            public T1 Object1 { get; set; }
+            public T2 Object2 { get; set; }
+            public IEnumerable<ValuePair> Mappings { get; set; }
+        }
+
+    }
+    public readonly struct ValuePair
+    {
+        public static ValuePair Create<T>(T prop1, T prop2)
+        {
+            return new ValuePair(prop1, prop2);
+        }
+        private ValuePair(object prop1, object prop2)
+        {
+            Prop1 = prop1;
+            Prop2 = prop2;
+        }
+
+        internal object Prop1 { get; }
+        internal object Prop2 { get; }
+
+        internal bool ValuesEqual()
+        {
+            if (Prop1 is null)
+                return Prop2 is null;
+
+            if (Prop2 is null)
+                return false;
+
+            return Prop1 == Prop2;
+        }
+    }
+
+}
---
+++ ObjectCompare/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using AutoBogus;
+using System.Collections.Generic;
+
+namespace ObjectCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualsTestSomeFields();
+            Console.WriteLine("----------------------");
+
+            EqualsTestAllFields();
+            Console.WriteLine("----------------------");
+
+            NotEqualsTestSomeFields();
+            Console.WriteLine("----------------------");
+
+            NotEqualsTestAllFields();
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Exit...");
+            Console.ReadLine();
+        }
+
+
+        static void EqualsTestAllFields()
+        {
+            PropertyModel model = EqualsModel;
+            IProperty entity = EqualsEntity;
+
+            var c = new ObjectComparer<PropertyModel, IProperty>(model, entity);
+
+            Console.WriteLine($"{nameof(EqualsTestAllFields)}: Objects Are Equal? = {c.Compare()}");
+        }
+
+        static void EqualsTestSomeFields()
+        {
+            PropertyModel model = EqualsModel;
+            IProperty entity = EqualsEntity;
+
+            var c = new ObjectComparer<PropertyModel, IProperty>(config =>
+            {
+                config.Object1 = model;
+                config.Object2 = entity;
+                config.Mappings = new List<ValuePair>
+                {
+                    ValuePair.Create(model.AddressLine1, entity.AddressLine1),
+                    ValuePair.Create(model.AddressLine2, entity.AddressLine2),
+                    ValuePair.Create(model.Phase, entity.Phase),
+                    ValuePair.Create(model.Id, entity.EntityId),
+                };
+            });
+
+            Console.WriteLine($"{nameof(NotEqualsTestSomeFields)}: Objects Are Equal? = {c.Compare()}");
+        }
+
+        static void NotEqualsTestAllFields()
+        {
+            PropertyModel model = RandomModel;
+            IProperty entity = RandomEntity;
+
+            var c = new ObjectComparer<PropertyModel, IProperty>(model, entity);
+
+            Console.WriteLine($"{nameof(NotEqualsTestAllFields)}: Objects Are Equal? = {c.Compare()}");
+        }
+
+        static void NotEqualsTestSomeFields()
+        {
+            PropertyModel model = RandomModel;
+            IProperty entity = RandomEntity;
+
+            var c = new ObjectComparer<PropertyModel, IProperty>(config =>
+            {
+                config.Object1 = model;
+                config.Object2 = entity;
+                config.Mappings = new List<ValuePair>
+                {
+                    ValuePair.Create(model.AddressLine1, entity.AddressLine1),
+                    ValuePair.Create(model.AddressLine2, entity.AddressLine2),
+                    ValuePair.Create(model.Phase, entity.Phase),
+                    ValuePair.Create(model.Id, entity.EntityId),
+                };
+            });
+
+            Console.WriteLine($"{nameof(NotEqualsTestSomeFields)}: Objects Are Equal? = {c.Compare()}");
+        }
+
+        static PropertyModel RandomModel => new AutoFaker<PropertyModel>().Generate();
+        static IProperty RandomEntity => new AutoFaker<PropertyEntity>().Generate();
+
+        static PropertyModel EqualsModel => new PropertyModel
+        {
+            Id = 10,
+            AddressLine1 = "foobar",
+            AddressLine2 = "barfoo",
+            Acreage = "10.2",
+            Block = null,
+            City = "The City",
+            County = "The County",
+            LegalDescription = "some desc",
+            Phase = "Two",
+            State = "NC",
+            Zip = "27603"
+        };
+
+        static IProperty EqualsEntity => new PropertyEntity
+        {
+            EntityId = 10,
+            AddressLine1 = "foobar",
+            AddressLine2 = "barfoo",
+            Acreage = "10.2",
+            Block = null,
+            City = "The City",
+            County = "The County",
+            LegalDescription = "some desc",
+            Phase = "Two",
+            State = "NC",
+            Zip = "27603"
+        };
+    }
+
+    class PropertyModel
+    {
+        public int Id { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string County { get; set; }
+        public string LegalDescription { get; set; }
+        public string Acreage { get; set; }
+        public string Block { get; set; }
+        public string Phase { get; set; }
+    }
+
+    interface IProperty
+    {
+        string Acreage { get; set; }
+        string AddressLine1 { get; set; }
+        string AddressLine2 { get; set; }
+        string Block { get; set; }
+        string City { get; set; }
+        string County { get; set; }
+        int EntityId { get; set; }
+        string LegalDescription { get; set; }
+        string Phase { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+    }
+
+    class PropertyEntity : IProperty
+    {
+        public int EntityId { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string County { get; set; }
+        public string LegalDescription { get; set; }
+        public string Acreage { get; set; }
+        public string Block { get; set; }
+        public string Phase { get; set; }
+    }
+}
